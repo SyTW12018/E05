61d333302793321da90445505f2ba9dba60f2369
alu0100904406 <alu0100904406@ull.edu.es>
2018-11-19 20:12:33 +0000

    refactorizando servidor

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 mongoose.connect('mongodb://admin:adminpass1@ds135413.mlab.com:35413/proyectostyw', {useNewUrlParser: true}, function(error){
 	if (error) {
@@ -26,6 +27,8 @@ var UserData = mongoose.model('UserData', userDataSchema);
 const app = express();
 const port = 8081;
 
+app.use(cors())
+
 app.use(express.static(__dirname + '/dist/project'));
 app.use(bodyParser());
 
@@ -47,9 +50,16 @@ app.get('/users',function(req, res){
 	});
 })
 
-app.get('/inicio_sesion',function(req, res){
+app.post('/inicio_sesion',function(req, res){
 	UserData.find({"usuario":req.body.usuario,"contrasena":req.body.contrasena }).select({ "nombre": 1,"usuario": 1, "contrasena": 1, "_id": 0}).exec(function (err, usuario) {
-		res.json(usuario);
+		if(usuario == []){
+			res.statusCode = 401;
+			res.send('Cannot Login');
+		}
+		else{
+			res.json(usuario);
+		}
+		
 	});
 })
 
@@ -58,6 +68,7 @@ app.get('/inicio_sesion',function(req, res){
 const server = http.createServer(app);
 
 server.listen(port,() => console.log('Running...'+port));
+
 
 
 
