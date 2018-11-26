5a9fbee97ae1c9d6a572215a8488762cac303182
alu0100904406 <alu0100904406@ull.edu.es>
2018-11-26 09:16:22 +0000

    Refactorizando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('project');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('Diario de clase como título', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to project!');
+    expect(compiled.querySelector('title').textContent).toContain('Diario de clase');
   });
 });
