eaedab249d91d686ddb0eb96e5805b4fae17d39a
alu0100904406 <alu0100904406@ull.edu.es>
2019-02-03 16:32:35 +0000

    arreglando foro

--- src/app/post-tema/post-tema.component.ts
+++ src/app/post-tema/post-tema.component.ts
@@ -12,24 +12,21 @@ export class PostTemaComponent implements OnInit {
   constructor(private httpService: HttpserviceService, private route: ActivatedRoute, private router: Router) { 
     this.httpService.get_post(this.route.snapshot.paramMap.get('id'), function(datospost) {
       this.post = datospost;
-      this.post.respuestas.sort(function (a,b){
-        a.fecha - b.fecha
-      });
     }.bind(this));
   }
 
   post;
   
   ngOnInit() {
+    this.httpService.get_post(this.route.snapshot.paramMap.get('id'), function(datospost) {
+      this.post = datospost;
+    }.bind(this));
   }
 
   responder(comentario){
     this.httpService.set_respuesta(this.post._id, comentario.value, function(){
       this.httpService.get_post(this.route.snapshot.paramMap.get('id'), function(datospost) {
         this.post = datospost;
-        this.post.respuestas.sort(function (a,b){
-          a.fecha - b.fecha
-        });
       }.bind(this));
     }.bind(this));
   }
